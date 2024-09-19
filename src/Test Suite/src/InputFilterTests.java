b17e9779445a092537a832adc15a11d0bbd89ac3
Aidan.Klug <aklug@luc.edu>
2024-09-19 08:37:00 -0500

    Seperated single test method in InputFilterTests into 3 disctinct methods for testing the creation of a Plateau, Rover, and movement command

--- src/Test Suite/src/InputFilterTests.java
+++ src/Test Suite/src/InputFilterTests.java
@@ -1,23 +1,49 @@
 import org.junit.Test;
 import org.junit.Assert;
 
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class InputFilterTests {
+    InputFilter inputFilterTestObject = new InputFilter();
     @Test
-    public void inputFilterTester() {
+    public void inputFilterPlateauTest() {
         int[] expected = {0,0};
-        InputFilter inputFilterTestObject = new InputFilter();
         //Create a new plateau of size 5 x 5
         inputFilterTestObject.inputIdentifier("5 5");
-        //Put a rover on the plateau at position 2 2 facing N
-        inputFilterTestObject.inputIdentifier("2 2 N");
-        //Move the rover left, then 2 spaces, then left again and 2 spaces.
-        inputFilterTestObject.inputIdentifier("LMMLMM");
-        //Expected output should be (0 0 S), Check Rover currPosition Array and currHeading Char
-        assertArrayEquals(expected, Rover.currPosition);
-        assertTrue('S'== Rover.currHeading);
+        //Confirm that the created Plateau has x and y values of 5 and 5 respectively.
+        assertEquals(5, Plateau.plateauXMax);
+        assertEquals(5, Plateau.plateauYMax);
 
     }
+
+    @Test
+    public void inputFilterRoverCreationTest() {
+        int[] expectedPosition = {2,2};
+
+        //Put a rover on the plateau at position 2 2 facing N
+        inputFilterTestObject.inputIdentifier("2 2 N");
+
+        //Expected output should be (0 0) and 'N', Check Rover currPosition Array and currHeading Char
+        assertArrayEquals(expectedPosition, Rover.currPosition);
+        assertEquals('N',Rover.currHeading);
+    }
+
+
+   @Test
+   public void inputFilterRoverMoveTest() {
+       int[] expectedPosition = {0,0};
+
+       //Put a rover on the plateau at position 2 2 facing N
+       inputFilterTestObject.inputIdentifier("2 2 N");
+
+       //Turn the rover left, then move 2 spaces, then turn left again and move 2 spaces.
+       inputFilterTestObject.inputIdentifier("LMMLMM");
+
+       //Expected output should be (0 0 S), Check Rover currPosition Array and currHeading Char
+       assertArrayEquals(expectedPosition, Rover.currPosition);
+       assertEquals('S', Rover.currHeading);
+
+
+   }
+
 }
