21cddcadc3e76b30e7d683e57094dd78a26b83a2
mcaballero <mcaballero3@luc.edu>
2024-09-20 11:53:34 -0500

    Added plateau dimensions test

--- src/Test Suite/src/PlateauTests.java
+++ src/Test Suite/src/PlateauTests.java
@@ -1,2 +1,20 @@
+import org.junit.Test;
+import org.junit.Assert;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
 public class PlateauTests {
+
+    @Test
+    public void plateauDimensionsTest() {
+        // Instantiate & populate plateau dimensions
+        ArrayList<Character> characterList = new ArrayList<>();
+        characterList.add('7');
+        characterList.add('7');
+        Plateau plateauTestObject = new Plateau(characterList);
+        // Expected dimensions are 7 x 7
+        int expected = 7;
+        assertEquals(expected, plateauTestObject.plateauXMax);
+        assertEquals(expected, plateauTestObject.plateauYMax);
+    }
 }
